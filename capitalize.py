35a4b5d7708eee66967d6dc7a163de425cf9b4f0
Yin10 <67246502+JimmyKim96@users.noreply.github.com>
2021-07-20 14:50:05 +0900

    Add files via upload

---
+++ capitalize.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+
+name = 'mtfk sob'
+info = 'hello mtfk?'
+_info = 'Sob, hello?'
+number_str = '5201314'
+
+new_name = name.capitalize()
+new_info = info.capitalize()
+_new_info= _info.capitalize()
+new_number_str = number_str.capitalize()
+
+print(new_name, name)
+print(new_number_str, number_str)
+
+#this
